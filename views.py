e1b974edbb53070cef9eca29bb5b814e326415ac
Priyanka Patel <128116001+Priyankapatel011@users.noreply.github.com>
2023-07-22 10:28:56 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    context = {
+        "variable1": "this is set",
+        "variable2": "this is priyanka"
+    }
+    return render(request, 'index.html', context)
+    # return HttpResponse("this is homepage")
+
+
+def about(request):
+    return render(request, 'about.html')
+    # return HttpResponse("this is about page")
+
+
+def services(request):
+    return render(request, 'services.html')
+    # return HttpResponse("this is services page")
+
+
+def contact(request):
+    return render(request, 'contact.html')
+    # return HttpResponse("this is contacts page")
+
+
+# user - admin
+# password - priyanka
